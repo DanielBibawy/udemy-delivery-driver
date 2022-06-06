3fdab76e6ae5f2a91f9209eb34ed6bca1c22e27f
DanielBibawy <75247553+DanielBibawy@users.noreply.github.com>
2022-06-06 14:21:28 +0100

    setting package pick up and drop off

--- Assets/Scripts/delivery.cs
+++ Assets/Scripts/delivery.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class delivery : MonoBehaviour
 {
+
+    public Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    public Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    bool hasPackage = false;
+    public float destroyDelay = 0.5f;
+
+    SpriteRenderer spriteRenderer;
+
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!");
     }
 
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        //package pick up
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if (other.tag == "Drop Off")
+
+        //package drop off
+        if (other.tag == "Drop Off" && hasPackage)
         {
             Debug.Log("Package Dropped!");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
